9022aa3f52743598e57f298d31c12a96c6cf5002
hadoop <hadoop@fancy-center.(none)>
2013-11-21 09:55:44 +0800

    fix anchor_info_data.xml error delete if

--- edu_info_day.sh
+++ edu_info_day.sh
@@ -3,7 +3,7 @@
 ANCHOR_INFO_DIR=`dirname "$0"`
 ANCHOR_INFO_DIR=`cd "$ANCHOR_INFO_DIR"; pwd`
 
-export ANT_OPTS="-Xmx1024M -Dlog4j.configuration=file://$ANCHOR_INFO_DIR/log4j.properties"
+export ANT_OPTS="-Xmx1024M -Dlog4j.configuration=file://$ANCHOR_INFO_DIR/edu_log.properties"
 #ant -lib $ANCHOR_INFO_DIR/antlib -listener org.apache.tools.ant.listener.Log4jListener -listener com.duowan.yy.ant.ext.AntExceptionListener -f $ANCHOR_INFO_DIR/get_data_test.xml get_anchor_info > /dev/null 2>&1
 
 ant -lib $ANCHOR_INFO_DIR/antlib -listener org.apache.tools.ant.listener.Log4jListener -listener com.duowan.yy.ant.ext.AntExceptionListener -f $ANCHOR_INFO_DIR/anchor_info_data.xml get_edu_info > /dev/null 2>&1
