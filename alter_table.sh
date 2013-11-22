434d3c89341808bd5375055411fd22be2ace8a9b
hadoop <hadoop@fancy-center.(none)>
2013-11-22 17:33:20 +0800

    delete unuseful scripts

--- alter_table.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/sh
-
-/data1/hadoop/wanghf/hcat/bin/hive -e "use datamine;alter table datamine_whf_anchor_info \\
-add columns (real_name string,profile string,resume string,agency_id bigint,agency_name string,teacher_type string,speciality string);"
--- get_invalid_uid.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-/data1/hadoop/wanghf/hcat/bin/hive -e "use datamine;select uid,anchor_type from datamine_whf_anchor_info where yyid is null and anchor_type<=2;" > invalid_uid_list
